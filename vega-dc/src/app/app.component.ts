0ac6c10c470a6ab39330c72bcca6cbef5b3bdbc7
Walk, Nicolas (EXTERN: Capgemini) <nicolas.walk@capgemini.com>
2019-07-26 09:50:22 +0200

    implemented datasource and initial implementation of a dataparser to parse data to vega cubes format

--- vega-dc/src/app/app.component.ts
+++ vega-dc/src/app/app.component.ts
@@ -1,25 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, Input, Output, ChangeDetectionStrategy, OnChanges, ChangeDetectorRef } from '@angular/core';
 import * as deck from '@deck.gl/core';
 import * as layers from '@deck.gl/layers';
 import * as luma from 'luma.gl';
 import * as vega from 'vega-lib';
 import * as SandDance from '@msrvida/sanddance';
 
+import {DataService} from './data.service';
+import {Dataparser} from './dataparser';
+
+
+type Instance = {
+    utilization: number;
+  }
+  
+  type Cluster = {
+    instances: Map<string, Instance>;
+    numInstances: number
+  }
+  
+  type Datacenter = {
+    clusters: Map<string, Cluster>;
+    numClusters: number,
+    numInstances: number
+  }
+  
+  type DCState = {
+    datacenters: Map<string, Datacenter>;
+    numDCs: number,
+    numClusters: number,
+    numInstances: number
+  }
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
-  
+
+    private temporalAxis = new Array<string>();
+    private timeSeries = new Map<string, DCState>();
+
+    private json: any = [];
+
+    constructor(private dataservice: DataService){
+        
+        this.dataservice.getInitialData()
+        .subscribe(logs => {
+            console.log(logs);
+            this.getData(logs)
+        })
+                
+  }
   title = 'vega-dc';
   
   ngOnInit(): void {
-    this.initCube();
+
   }
 
-  initCube(): void{
+  initCube(cubedata:any): void{
+
     var cubeTest;
+    console.log(cubedata);
     (function (cubeTest) {
       SandDance.use(vega, deck, layers, luma);
         var colors = {
@@ -30,23 +75,8 @@ export class AppComponent implements OnInit {
         };
         cubeTest.presenter = new SandDance.VegaDeckGl.Presenter(document.querySelector('#vis'));
         var stage = {
-            cubeData: [
-                {
-                    color: colors.red,
-                    position: [0, 0, 0],
-                    size: [100, 100, 100]
-                },
-                {
-                    color: colors.green,
-                    position: [110, 0, 0],
-                    size: [100, 100, 150]
-                },
-                {
-                    color: colors.blue,
-                    position: [220, 0, 0],
-                    size: [100, 100, 300]
-                }
-            ],
+            cubeData: cubedata
+            ,
             legend: { rows: {} },
             axes: {
                 x: [{
@@ -72,17 +102,26 @@ export class AppComponent implements OnInit {
             view: "3d",
             
         };
-        cubeTest.presenter.present(stage, 200, 400);
+        cubeTest.presenter.present(stage, 0, 1);
         var orbitViewState = {
             distance: 10,
             fov: 60,
             lookAt: [90, 15, 23, 1],
-            rotationOrbit: -45,
-            rotationX: 67,
+            rotationOrbit: 0,
+            rotationX: 0,
             zoom: 0.01
         };
         cubeTest.presenter.deckgl.setProps({ viewState: orbitViewState });
     })(cubeTest || (cubeTest = {}));
   }
 
+  getData(json){
+
+    let parser = new Dataparser();
+    parser.receivedData(json)
+    var cubedata = parser.parseToVega(10);
+    this.initCube(cubedata)
+  }
+
+  
 }
