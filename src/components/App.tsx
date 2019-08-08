6b0039ae0368f1c15de82d4318034fd210d5c8a4
Sultanow <sultanow@outlook.com>
2019-08-08 19:53:30 +0200

    placed SolrCoreSelector into App

---
+++ src/components/App.tsx
@@ -0,0 +1,109 @@
+import React, { createRef } from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Row } from 'react-bootstrap';
+import './App.css';
+import Sidebar from './components/Sidebar'
+import CubesVisualisation from './components/CubesVisualisation';
+import DataSources from './components/DataSources';
+import Topbar from './components/Topbar';
+import SolrDataService from './util/SolrDataService';
+import SolrAdapter from './util/SolrAdapter';
+import DCState from './model/DCState'
+import SolrCoreSelector from './components/solr/SolrCoreSelector'
+
+interface AppState {
+  statusMessage: string;
+  logData: [];
+  startDateTime: string;
+  endDateTime: string;
+  dataSource: string;
+  selectedSolrCore: string;
+  selectedPointInTime: number;
+  temporalAxis: string[],
+  timeSeries: Map<string, DCState>
+  grid: Map<string, Array<number>>
+  maxH: number;
+}
+
+class App extends React.Component<{}, AppState> {
+
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      statusMessage: null,
+      logData: [],
+      // Set the default boundaries of the log data to be displayed
+      startDateTime: '2018-08-03T00:00:00Z', // TODO: replace with date from 2 week ago or something similiar
+      endDateTime: new Date().toISOString().split('.')[0] + 'Z', // today's date e.g. 2019-08-05T12:06:45Z
+      dataSource: 'solr',
+      selectedSolrCore: 'test',
+      selectedPointInTime: 0,
+      temporalAxis: [],
+      timeSeries: new Map(),
+      grid: new Map(),
+      maxH: 0
+    };
+  }
+
+  componentDidMount() {
+    // get initial log data based on default values
+    this.getLogData(this.state.startDateTime, this.state.endDateTime, this.state.selectedSolrCore);
+  }
+
+  getLogData = (startDateTime: string, endDateTime: string, selectedSolrCore: string) => {
+    // TODO: implement other data sources
+    let dataService = new SolrDataService();
+    dataService.getLogDataFromSolr(startDateTime, endDateTime, selectedSolrCore).then((data: any) => {
+      // TODO: call dataparser from util folder in order to parse the log data
+      const solrAdapter = new SolrAdapter();
+      // console.log(data.data);
+      solrAdapter.receivedData(data.data);
+      this.setState({
+        // there is a bug, the last element ist undefined
+        temporalAxis: solrAdapter.temporalAxis,
+        timeSeries: solrAdapter.timeSeries,
+        grid: solrAdapter.grid,
+        maxH: solrAdapter.maxh,
+      });
+
+    }).catch((error: any) => console.log(error));
+  }
+
+  child = createRef<CubesVisualisation>();
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Sidebar />
+          <SolrCoreSelector />
+          <Topbar />
+            <Row>
+              <Route
+                path="/datasources"
+                render={props => <DataSources {...props} dataSource={this.state.dataSource} selectedSolrCore={this.state.selectedSolrCore} />}
+              />
+              <Route exact
+                path="/"
+                render={props => <CubesVisualisation {...props} ref={this.child} data={this.state.timeSeries.get(this.state.temporalAxis[this.state.selectedPointInTime])} grid={this.state.grid} />}
+              />
+                <div className="slidercontainer">
+                  <input type="range" min="0" max={this.state.temporalAxis.length-2} className="slider" id="myRange" value={this.state.selectedPointInTime} onChange={this.accesChild} />
+                  <p>{this.state.temporalAxis[this.state.selectedPointInTime]}</p>
+                </div>
+            </Row>      
+        </div>
+      </BrowserRouter>
+    );
+  };
+
+  accesChild = (event) => {
+    this.setState({ selectedPointInTime: event.target.value });
+    // if (this.child.current) {
+    //   this.child.current.randomnizeBarHeights();
+    // }
+  }
+
+}
+
+
+export default App;
