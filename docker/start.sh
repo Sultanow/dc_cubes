85c2fbbf76345d74d93091e1be312e16c6207909
Daniel Friedmann <mail@dfriedmann.de>
2019-08-11 23:21:35 +0200

    Add CSV importer

--- docker/start.sh
+++ docker/start.sh
@@ -5,38 +5,40 @@
 
 # Variables
 DOCKER_NETWORK=dc_cubes_network
-DC_CUBES_NAME=dc_cubes
+DC_CUBES=dc_cubes
+DC_CUBES_SOLR=dc_cubes_solr
 NGINX_HOST_PORT=80
 SOLR_HOST_PORT=8983
-SOLR_CORE_NAME=test
+SOLR_CORE_NAME=dc_cubes
 
 # 0. Clean up - just in case we have some containers already running
-docker stop dc_cubes && docker stop dc_cubes_solr
-docker rm dc_cubes && docker rm dc_cubes_solr
+docker stop $DC_CUBES && docker rm $DC_CUBES # Stop dc_cubes
+docker stop $DC_CUBES_SOLR && docker rm $DC_CUBES_SOLR # Stop dc_cubes_solr
+docker network rm $DOCKER_NETWORK
 
 # 1. Build dc_cubes
 echo "Building dc_cubes Docker image..."
-docker build -f dc_cubes/Dockerfile -t $DC_CUBES_NAME:v1 .
+docker build --no-cache -f dc_cubes/Dockerfile -t $DC_CUBES:v1 .
 
 # 2. Create Docker network
 echo "Creating docker network for dc_cubes..."
-docker network create $DOCKER_NETWORK
+docker network create dc_cubes_network --driver bridge
 
 # 3. Run dc_cubes in created network
 echo "Spin up dc_cubes container..."
-docker run --network $DOCKER_NETWORK -d --name $DC_CUBES_NAME -p $NGINX_HOST_PORT:80 $DC_CUBES_NAME:v1
+docker run --network $DOCKER_NETWORK --name $DC_CUBES -d -p $NGINX_HOST_PORT:80 $DC_CUBES:v1
 
 # 4. Run Solr
 echo "Spin up solr"
-docker run --network $DOCKER_NETWORK -d -e SOLR_CORE=$SOLR_CORE_NAME --name dc_cubes_solr -p $SOLR_HOST_PORT:8983 bitnami/solr:latest
+docker run --name dc_cubes_solr --network dc_cubes_network -d -e SOLR_CORE=dc_cubes -p 8983:8983 bitnami/solr:latest
 
 # 5. Import sample infrastructure data
-docker build -f ./solr_importer/Dockerfile -t solr_importer:v1 .
+docker build --no-cache -f ./solr_importer/Dockerfile -t solr_importer:v1 .
+echo "Wait until solr core is ready"
+sleep 20 # Todo - check status of core in loop
+docker run --rm --network dc_cubes_network --name dc_cubes_solr_testdata_importer solr_importer:v1
 
-# TODO Run importer with connection to dc_cubes_solr
-
-# docker run --network $DOCKER_NETWORK --rm byrnedo/alpine-curl http://dc_cubes_solr:8983/solr/test/select?q=*%3A*
-# docker run --network $DOCKER_NETWORK --rm --name dc_cubes_solr_testdata_importer solr_importer:v1
+# docker run --rm --network dc_cubes_network --name curl-test  byrnedo/alpine-curl http://dc_cubes_solr:8983/solr/dc_cubes/schema -i
 
 echo "Done."
 echo "Navigate to http://localhost in your browser."
