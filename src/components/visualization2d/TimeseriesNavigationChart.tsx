ad23a6cf54fc67b643e201242933c56a8496ce78
Ivan Levarda <ivan.levarda@outlook.de>
2019-08-20 14:07:58 +0200

    Add Styling to the TimeSeriesNavigationChart and add a second line

--- src/components/visualization2d/TimeseriesNavigationChart.tsx
+++ src/components/visualization2d/TimeseriesNavigationChart.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import * as d3 from 'd3';
+import './TimeseriesNavigationChart.css';
 
 export default class TimeseriesNavigationChart extends Component {
 
@@ -14,7 +15,18 @@ export default class TimeseriesNavigationChart extends Component {
             { day: '02-15-2016', count: 140 },
             { day: '02-16-2016', count: 380 },
             { day: '02-17-2016', count: 100 },
-            { day: '02-18-2016', count: 150 }
+            { day: '02-18-2016', count: 150 },
+            
+        ];
+        var data2 = [
+            { day: '02-11-2016', count: 250 },
+            { day: '02-12-2016', count: 150 },
+            { day: '02-13-2016', count: 300 },
+            { day: '02-14-2016', count: 170 },
+            { day: '02-15-2016', count: 300 },
+            { day: '02-16-2016', count: 34 },
+            { day: '02-17-2016', count: 260 },
+            { day: '02-18-2016', count: 80 },
         ];
 
         var margin = { top: 20, right: 20, bottom: 0, left: 0 }
@@ -42,6 +54,12 @@ export default class TimeseriesNavigationChart extends Component {
             .y0(y(0))
             .y1(function (d) { return y(d.count); });
 
+        var area2 = d3.area<TimeNavData>()
+            .curve(d3.curveMonotoneX)
+            .x(function (d) { return x(parseDate(d.day)); })
+            .y0(y(0))
+            .y1(function (d) { return y(d.count); });
+
 
         var transform = 'translate(' + margin.left + ',' + margin.top + ')';
 
@@ -49,7 +67,8 @@ export default class TimeseriesNavigationChart extends Component {
             <div>
                 <svg id={"52235"} width={900} height={250}>
                     <g transform={transform}>
-                        <path className="line shadow" d={area(data)} strokeLinecap="round" />
+                        <path className="area-max" d={area(data)} strokeLinecap="round" />
+                        <path className="area-avg" d={area2(data2)} strokeLinecap="round" />
                     </g>
                 </svg>
             </div>
