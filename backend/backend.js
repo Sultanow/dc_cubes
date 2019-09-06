01bf9fb33d85a0bab5fb5696b1ab0fd772d610f3
Sebastian Steindl <sebstein10@gmail.com>
2019-09-06 17:00:59 +0200

    Backend: Refactoring.

--- backend/backend.js
+++ backend/backend.js
@@ -13,77 +13,21 @@ app.get("/", function (req, res) {
     res.send("node is online");
 });
 
-app.listen(8080, function () {
-    console.log("NodeServer listening on Port " + port);
-});
-
-app.get("/historicalData/:from/:to", function (req, res) {
+app.get("/historical/:from/:to", function (req, res) {
     let solrQuery = getQueryHistorical(req.params.from, req.params.to);
-    // TODO: Refactor to avoid code duplication at forecast
-    var options = {
-        host: '127.0.0.1',
-        port: "8983",
-        path: solrQuery
-    };
-
-    var queryResult = null;
-    let requestSolr = http.get(options, function (response) {
-        // Buffer the body entirely for processing as a whole.
-        var bodyChunks = [];
-        response.on('data', function (chunk) {
-            // You can process streamed parts here...
-            bodyChunks.push(chunk);
-        }).on('end', function () {
-            queryResult =  Buffer.concat(bodyChunks);
-            if(queryResult) {
-                res.status(200);
-                res.write(queryResult);
-                res.send();
-            }
-            else {
-                res.status(404);
-                res.send();
-            }
-        })
-    });
-    requestSolr.on('error', function (e) {
-        console.log('historicalData -> query Solr, ERROR: ' + e.message);
-    });
-
+    // TODO: should from & to be query params to make them optional?
+    querySolr(solrQuery, res);
 });
 
 app.get("/forecast/:from/:to", function (req, res) {
     let solrQuery = getQueryForecast(req.params.from, req.params.to);
-    var options = {
-        host: '127.0.0.1',
-        port: "8983",
-        path: solrQuery
-    };
-
-    var queryResult = null;
-    let requestSolr = http.get(options, function (response) {
-        // Buffer the body entirely for processing as a whole.
-        var bodyChunks = [];
-        response.on('data', function (chunk) {
-            // You can process streamed parts here...
-            bodyChunks.push(chunk);
-        }).on('end', function () {
-            queryResult =  Buffer.concat(bodyChunks);
-            if(queryResult) {
-                res.statusCode(200);
-                res.write(queryResult);
-                res.send();
-            }
-            else {
-                res.statusCode(404);
-                res.send();
-            }
-        })
-    });
-    requestSolr.on('error', function (e) {
-        console.log('historicalData -> query Solr, ERROR: ' + e.message);
-    });
+    querySolr(solrQuery, res);
 });
+
+app.listen(port, function () {
+    console.log("NodeServer listening on Port " + port);
+});
+
 
 // TODO Personalize query / change query params
 function getQueryHistorical(params) {
@@ -91,4 +35,38 @@ function getQueryHistorical(params) {
 }
 function getQueryForecast(params) {
     return `/solr/${forecast}/query?q=*:*&start=0&rows=30000`;
+}
+
+
+function querySolr(queryString, originalRes ) {
+    const options = {
+        host: '127.0.0.1',
+        port: "8983",
+        path: queryString
+    };
+
+    let queryResult = null;
+    let requestSolr = http.get(options, function (response) {
+        // Buffer the body entirely for processing as a whole.
+        let bodyChunks = [];
+        response.on('data', function (chunk) {
+            // You can process streamed parts here...
+            bodyChunks.push(chunk);
+        }).on('end', function () {
+            queryResult =  Buffer.concat(bodyChunks);
+            if(queryResult) {
+                originalRes.status(200);
+                originalRes.write(queryResult);
+                originalRes.send();
+            }
+            else {
+                originalRes.status(404);
+                originalRes.send();
+            }
+        })
+    });
+    requestSolr.on('error', function (e) {
+        console.log('historicalData -> query Solr, ERROR: ' + e.message);
+    });
+
 }
