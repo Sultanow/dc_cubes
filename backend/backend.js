d43041ac30436570ec32555f757f38327084190e
Sebastian Steindl <sebstein10@gmail.com>
2019-09-06 16:55:45 +0200

    Query Solr from Node

--- backend/backend.js
+++ backend/backend.js
@@ -1,12 +1,13 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var path = require("path");
-var express = require("express");
-var app = express();
-var dir = path.join(__dirname, 'public');
+const http = require('http');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const config = require("./nodeConfig.json");
 
-const port = 8080;
+const port = config.port;
+const historical = config.historicalDataCore;
+const forecast = config.forecastDataCore;
 
 app.get("/", function (req, res) {
     res.send("node is online");
@@ -16,83 +17,78 @@ app.listen(8080, function () {
     console.log("NodeServer listening on Port " + port);
 });
 
-app.get("/historicalData", function (req, res) {
-    let solrQuery = ""//req.
-    if (!queryIsSelect()) {
-        res.writeHead(403);
-        res.setHeader('Content-Type', 'text/plain');
-    }
-    else {
-        req.on("end", function () {
-            try {
-                querySolr(solrQuery, function (result) {
-                    res.writeHead(200);
-                    // since its a http result object and the function param is also called result,
-                    //you have to call result.result to get what you would expect
-                    res.write(JSON.stringify(result.result));
-                    res.end();
-                });
-            }
-            catch (e) {
-                console.log(e);
-                res.end();
-            }
-
-        });
-    }
-    return res.end('Forbidden');
-    //res.send("Responding with historical Data from Solr");
-});
-
-app.get("/forecast", function (req, res) {
-    res.send("Responding with forecast Data from Solr");
-});
-
-
-function queryIsSelect(solrQuery) {
-    // check if the query is a select query, otherwise (update, delete) stop handling the request
-    let indexOfMarker = solrQuery.indexOf("?");
-    if (indexOfMarker < 0) { // cancel if "?" doesnt exist in solrQuery
-        return false;
-    }
-    let solrAction = (solrQuery.substr(0, indexOfMarker));
-    solrAction = solrAction.substr(solrAction.lastIndexOf("/") + 1);
-
-    return (solrAction === "select");
-}
-
-function querySolr(query, callback) {
-
-    return http.get({
+app.get("/historicalData/:from/:to", function (req, res) {
+    let solrQuery = getQueryHistorical(req.params.from, req.params.to);
+    // TODO: Refactor to avoid code duplication at forecast
+    var options = {
         host: '127.0.0.1',
         port: "8983",
-        path: query
-    }, function (response) {
-        // Continuously update stream with data
-        var body = '';
-        // build up the result chunk by chunk
-        response.on('data', function (chunk) {
-            body += chunk;
-        });
-        // when all chunks arrived:
-        response.on('end', function () {
+        path: solrQuery
+    };
 
-            // json has to be parsed
-            try {
-                var parsed = JSON.parse(body);
+    var queryResult = null;
+    let requestSolr = http.get(options, function (response) {
+        // Buffer the body entirely for processing as a whole.
+        var bodyChunks = [];
+        response.on('data', function (chunk) {
+            // You can process streamed parts here...
+            bodyChunks.push(chunk);
+        }).on('end', function () {
+            queryResult =  Buffer.concat(bodyChunks);
+            if(queryResult) {
+                res.status(200);
+                res.write(queryResult);
+                res.send();
             }
-            catch (e) {
-                console.log("error parse", e);
-                parsed = ("{}");
+            else {
+                res.status(404);
+                res.send();
             }
-            // call the callback function with the parsed solrquery result
-            callback({
-                result: parsed
-            });
-        });
-    })
-        .on("error", function (e) {
-            console.log("Error while querying Solr (is it running?): ", e);
-            return {};
-        });
+        })
+    });
+    requestSolr.on('error', function (e) {
+        console.log('historicalData -> query Solr, ERROR: ' + e.message);
+    });
+
+});
+
+app.get("/forecast/:from/:to", function (req, res) {
+    let solrQuery = getQueryForecast(req.params.from, req.params.to);
+    var options = {
+        host: '127.0.0.1',
+        port: "8983",
+        path: solrQuery
+    };
+
+    var queryResult = null;
+    let requestSolr = http.get(options, function (response) {
+        // Buffer the body entirely for processing as a whole.
+        var bodyChunks = [];
+        response.on('data', function (chunk) {
+            // You can process streamed parts here...
+            bodyChunks.push(chunk);
+        }).on('end', function () {
+            queryResult =  Buffer.concat(bodyChunks);
+            if(queryResult) {
+                res.statusCode(200);
+                res.write(queryResult);
+                res.send();
+            }
+            else {
+                res.statusCode(404);
+                res.send();
+            }
+        })
+    });
+    requestSolr.on('error', function (e) {
+        console.log('historicalData -> query Solr, ERROR: ' + e.message);
+    });
+});
+
+// TODO Personalize query / change query params
+function getQueryHistorical(params) {
+    return `/solr/${historical}/query?q=*:*&start=0&rows=30000`;
+}
+function getQueryForecast(params) {
+    return `/solr/${forecast}/query?q=*:*&start=0&rows=30000`;
 }
