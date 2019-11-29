ebf5f9397432cac3943639bf38dfea5aff824b8f
Sebastian Steindl <sebstein10@gmail.com>
2019-11-29 12:19:15 +0100

    Backend: Update pushRow()

--- backend/MLSkript.py
+++ backend/MLSkript.py
@@ -2,8 +2,6 @@ import requests
 import pandas as pd
 import json
 import csv
-import random
-import matplotlib.pyplot as plt
 import pickle
 
 def pushData(row):
@@ -46,77 +44,7 @@ def pushData(row):
 
 if __name__ == "__main__":
     print("MLSkript.py ausgeführt")
-    # mockupData = pd.read_csv("../../data/data.dsv", header=0, sep="|", index_col=0, low_memory=False)
-    # unwantedCols = ["host","cluster","dc","perm","instanz","verfahren","service","response","minv","maxv","avg","var","dev_upp","dev_low","perc90","perc95","perc99.9","sum","sum_of_squares", "server"]
-    # mockupData.drop(unwantedCols, axis=1, inplace=True)
-    # mockupData.plot(x="timestamp", y="count")
-    # plt.show()
-
-    filename = "../../data/data.dsv"
-    chunksize = 100000
-    datatypes = {
-        "TARGET_NAME": object,
-        "TARGET_TYPE": object,
-        "TARGET_GUID": object,
-        "METRIC_NAME": object,
-        "METRIC_TYPE": int,
-        "METRIC_COLUMN": object,
-        "METRIC_GUID": object,
-        "METRIC_LABEL": object,
-        "COLUMN_LABEL": object,
-        "COLLECTION_TIMESTAMP": object,
-        "KEY_VALUE": object,
-        "KEY_VALUE2": object,
-        "KEY_VALUE3": object,
-        "KEY_VALUE4": object,
-        "KEY_VALUE5": object,
-        "KEY_VALUE6": object,
-        "KEY_VALUE7": object,
-    }
-
-    colsWithDates = ["COLLECTION_TIMESTAMP"]
-    counter = 0
-
-    pd.set_option('display.max_colwidth', -1)
-    pd.set_option('display.max_columns', None)
-
-    irrelevantColumns = ["KEY_VALUE",
-                      "KEY_VALUE2",
-                      "KEY_VALUE3",
-                      "KEY_VALUE4",
-                      "TARGET_TYPE",
-                      "KEY_VALUE5",
-                      "KEY_VALUE6",
-                      "METRIC_NAME",
-                      "METRIC_TYPE",
-                      "KEY_VALUE7"]
-
-    abbreviatedFeatures = []
-    fullFeatures = []
-    for chunk in pd.read_csv(filename, chunksize=chunksize, sep="|", encoding="latin1", parse_dates=colsWithDates,
-                             dtype=datatypes):
-
-        abbNames = chunk["METRIC_COLUMN"].unique()
-        fullNames= chunk["COLUMN_LABEL"].unique()
-
-        for name in abbNames:
-            if name not in abbreviatedFeatures:
-                abbreviatedFeatures.append(name)
-        for name in fullNames:
-            if name not in fullFeatures:
-                fullFeatures.append(name)
-
-        # chunk.drop(irrelevantColumns, axis="columns", inplace=True)
-        counter = counter + 1
-        print("\n\n ++++chunk NR: %5d ++++" % counter)
-        # print("head: ", chunk.head(1))
-
-    with open("listObject.pkl", "wb") as pickleFile:
-        pickle.dump([abbreviatedFeatures, fullFeatures], pickleFile)
-
-    with open("features.csv", 'w', newline='') as featuresFile:
-        writer = csv.writer(featuresFile, delimiter=",")
-        writer.writerow(["shortName", "fullName"])
-
-        for index, row in enumerate(abbreviatedFeatures):
-            writer.writerow([row, fullFeatures[index]])
+    filePath = "../predictions.csv"
+    df = pd.read_csv(filePath, sep=",", encoding="latin1")
+    df.apply(pushData, axis=1)
+    print("Data was pushed to Solr/dc_cubes_forecast.")
