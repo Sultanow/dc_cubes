124d91a5c32a1a17d7233a4e92ad107757a630e0
Sebastian Steindl <sebstein10@gmail.com>
2019-12-04 17:34:53 +0100

    Add Solr-Functions to MLSkript.py

--- backend/MLSkript.py
+++ backend/MLSkript.py
@@ -5,8 +5,10 @@ import csv
 import pickle
 
 def pushData(row):
+    global counter
+    global coreName
     # defining the api-endpoint
-    url = "http://localhost:8983/solr/dc_cubes_forecast/update/json?commit=true&wt=json"
+    url = "http://localhost:8983/solr/pyTest/update/json?wt=json"
 
     # data to be sent to api
     data = {
@@ -40,11 +42,53 @@ def pushData(row):
     headers = {'Content-type': 'application/json'}
     # sending post request
     requests.post(url=url, data=json.dumps(data), headers=headers)
+    counter += 1
+    if (counter % 1000 == 0):
+        print("Commiting... counter:", counter)
+        requests.post("http://localhost:8983/solr/"+coreName+"/update?commit=true")
 
+def createSolrCore(coreName):
+    url = "http://localhost:8983/solr/admin/cores?action=CREATE&name="+coreName+"&configSet=_default"
+    requests.post(url = url)
+    print(coreName, " created")
+
+"""
+if deleteEverything = True all files associated with the core are deleted aswell.
+See: https://lucene.apache.org/solr/guide/6_6/coreadmin-api.html#CoreAdminAPI-UNLOAD
+"""
+def deleteSolrCore(coreName, deleteEverything = False):
+    url = "http://localhost:8983/solr/admin/cores?action=UNLOAD&core="+coreName
+    if (deleteEverything):
+        url += "&deleteInstanceDir=true"
+    requests.get(url)
+    print(coreName, " deleted")
+
+def initSchema(coreName):
+    url = "http://localhost:8983/solr/"+coreName+"/schema"
+    headers = {'Content-type': 'application/json'}
+    rowsDict =  {
+        "timestamp": "pdate", "host": "string", "cluster": "pint", "dc": "pint", "perm": "pint", "instanz": "string", "verfahren": "string",
+        "service": "string", "response": "pint", "count": "pint", "minv": "pint", "maxv": "pint", "avg": "pfloat", "var": "pfloat",
+        "dev_upp": "pfloat", "dev_low": "pfloat", "perc90": "pfloat", "perc95": "pfloat", "perc99": "pfloat", "sum": "pint",
+        "sum_of_squares": "pint", "server": "string"}
+
+    for name in rowsDict:
+        data = {
+            "add-field":{"stored": "true","docValues": "true","indexed": "false", "multiValued": "false", "name":name,"type":rowsDict[name]}
+        }
+        requests.post(url=url, data=json.dumps(data), headers=headers)
+    print(coreName, " schema inited")
+    
 
 if __name__ == "__main__":
     print("MLSkript.py ausgeführt")
+    counter = 0;
     filePath = "C:/Users/Sebastian/Desktop/Uni/_Bachelorarbeit/data/PresentationPrediction.csv"
-    df = pd.read_csv(filePath, sep=",", encoding="latin1")
-    df.apply(pushData, axis=1)
-    print("Data was pushed to Solr/dc_cubes_forecast.")
+    coreName = "dc_cubes_forecast"
+
+    # deleteSolrCore(coreName)
+    # createSolrCore(coreName)
+    # initSchema(coreName)
+
+    #df = pd.read_csv(filePath, sep=",", encoding="latin1")
+    #df.apply(pushData, axis=1)
