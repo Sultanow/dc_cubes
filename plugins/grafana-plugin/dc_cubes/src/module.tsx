f23c9702ab94e0b80288934de8955079f089e8d7
Roland Odorfer <r.odorfer1997@gmx.de>
2020-01-10 23:53:08 +0100

    Adapt grafana-plugin instantiation

--- plugins/grafana-plugin/dc_cubes/src/module.tsx
+++ plugins/grafana-plugin/dc_cubes/src/module.tsx
@@ -1,12 +1,4 @@
-import React, { PureComponent } from 'react';
-import { PanelProps, PanelPlugin } from '@grafana/ui';
-import './index.css';
-import App from './App';
+import { PanelPlugin } from '@grafana/data/';
+import { App } from './App';
 
-export class MyPanel extends PureComponent<PanelProps> {
-  render() {
-    return <App />;
-  }
-}
-
-export const plugin = new PanelPlugin(MyPanel);
+export const plugin = new PanelPlugin(App);
