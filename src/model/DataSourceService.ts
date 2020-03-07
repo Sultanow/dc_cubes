0b11e61193c4f708304826cf576c8cf17b776ce9
Burak <bu.kayaalp@outlook.de>
2020-03-07 15:39:01 +0100

    wip

--- src/model/DataSourceService.ts
+++ src/model/DataSourceService.ts
@@ -5,15 +5,9 @@ interface DataSourceService {
     getAllHistorical: () => Promise<object>
     getAggregatedLogData: (from: string, to: string, selectedMeasure: string, aggregationType: AggregationType) => Promise<object>
     getForecast: (from: string, to: string) => Promise<object>
-<<<<<<< HEAD
     getMaxValueOfTwoCores: (from: string, to: string, selectedMeasure: string) => Promise<object>
     getDistinctTimestamps: (idex: string) => Promise<number[]>
     getAggregatedValueForEachTimestamp: (selectedMeasure: string, aggregationType: AggregationType, esIndex: string) => Promise<object>
-=======
-    getMaxValueOfTwoCores: (from: string, to: string, selectedMeasure: string) => Promise<number>
-    getDistinctTimestamps: (core: string) => Promise<string[]>
-    getAggregatedValueForEachTimestamp: (selectedMeasure: string, aggregationType: AggregationType, solrCore: string) => Promise<object>
->>>>>>> master
 }
 
 export default DataSourceService
